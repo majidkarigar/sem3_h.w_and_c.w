caf3922bd4992d154366cf05a41a60df3ab4ac41
majidkarigar <majidkarigar123@gmail.com>
2026-07-01 20:24:54 +0530

    manual created folder
    1 question is done

---
+++ python/1ds.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  1 19:14:27 2026
+
+@author: LENOVO
+"""
+
+l1 = []
+l1=["A"]*30
+print(l1)
+s=int(input("Entr the roll no : "))
+l1[s-1]="P"
+print(l1)
